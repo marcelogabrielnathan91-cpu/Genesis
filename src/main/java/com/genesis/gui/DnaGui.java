d8df018176885b96c75613dd957a4c5448b277fd
marcelogabrielnathan91-cpu <marcelogabrielnathan91@gmail.com>
2026-05-05 15:37:34 +0800

    Update DnaGui.java

--- src/main/java/com/genesis/gui/DnaGui.java
+++ src/main/java/com/genesis/gui/DnaGui.java
@@ -1,1 +1,27 @@
+// gui/DnaGui.java
+package com.genesis.gui;
+
+import com.genesis.Genesis;
+import com.genesis.dna.DnaManager;
+import com.genesis.dna.DnaType;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.persistence.PersistentDataType;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+public class DnaGui {
+
+    public static final String GUI_TITLE = ChatColor.DARK_AQUA + "✧ DNA Transplanter ✧";
+
+    public static Inventory createGui(Player player, Genesis plugin) {
+        Inventory gui = Bukkit.createInventory(null, 54, GUI_TITLE);
+        D
 
